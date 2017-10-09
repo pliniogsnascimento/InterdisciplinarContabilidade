c386434ec4fda127daefdb5406ec3b4d662e0245
Plinio Nascimento <plinio.gsnascimento@outlook.com>
2017-10-09 12:51:43 -0300

    Corrigido acesso ao MySQL

--- src/main/java/com/fatec/interdisciplinar/controllers/Hello.java
+++ src/main/java/com/fatec/interdisciplinar/controllers/Hello.java
@@ -1,11 +1,21 @@
 package com.fatec.interdisciplinar.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fatec.interdisciplinar.model.Funcionario;
+import com.fatec.interdisciplinar.service.FuncionarioService;
 
 @Controller
 public class Hello {
+	
+	@Autowired
+	FuncionarioService funcionarioService;
 	
 	@RequestMapping(value="/")
 	public String hello() {
@@ -25,5 +35,27 @@ public class Hello {
 	@RequestMapping(value = "/CadastroFuncionario")
 	public String cadastroFuncionario() {
 		return "cadastro-funcionario.html";
+	}
+	
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	@ResponseBody
+	public void getAll() {
+		String message = null;
+		Funcionario funcionario1 = new Funcionario();
+		Funcionario funcionario2 = new Funcionario();
+		
+		funcionario1.setNome("José");
+		funcionario2.setNome("Plinio");
+		
+		this.funcionarioService.salvar(funcionario1);
+		this.funcionarioService.salvar(funcionario2);
+		
+		//List<Funcionario> funcionarios = this.funcionarioService.finAll();
+		
+		//for(Funcionario funcionario:funcionarios) {
+			//message = funcionario.toString() + "\n";
+		//}
+		
+		//return message;
 	}
 }
--- src/main/java/com/fatec/interdisciplinar/model/Funcionario.java
+++ src/main/java/com/fatec/interdisciplinar/model/Funcionario.java
@@ -14,4 +14,22 @@ public class Funcionario {
 	private Long id;
 	private String nome;
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public String toString() {
+		return "Funcionario [id=" + id + ", nome=" + nome + "]";
+	}
+	
 }
